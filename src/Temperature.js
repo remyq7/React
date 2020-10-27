df11d7cbcc45b61d27eddf0f5f8ea215f641533c
remyq7 <72361098+remyq7@users.noreply.github.com>
2020-10-26 20:57:43 -0400

    Create Description.js Edit Temperature.js to include Description.js

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Humidity from "./Humidity.js";
 import Wind from "./Wind.js";
+import Description from "./Description.js";
 
 export default function Temperature(props) {
   return (
     <div className="Temperature">
       <h2> {props.value}°C | °F</h2>
+      <Description value={props.description} />
       <Humidity value={props.humidity} />
       <Wind value={props.wind} />
     </div>
