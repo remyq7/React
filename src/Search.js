d962551985547a68c3d7fa4788abb6740ab4b89b
remyq7 <72361098+remyq7@users.noreply.github.com>
2020-10-26 21:26:39 -0400

    add placeholder to search bar and edit css

--- src/Search.js
+++ src/Search.js
@@ -4,7 +4,7 @@ export default function Search() {
   return (
     <div className="Search">
       <form className="Search">
-        <input type="Search" />
+        <input type="Search" placeholder="Type city here" />
         <div className="button">
           <input type="submit" value="Search" />
         </div>
